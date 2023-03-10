984eb455cacbf31a6ba345e5fce19e21763dc1b1
Olga <olganedelcuam@gmail.com>
2023-03-11 00:27:51 +0100

    Blogging page

---
+++ error_handling/src/components/Home.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList.js";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs");
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
+
+  return <div>
+    { isPending && <div>Loading...</div>}
+    { blogs && <BlogList blogs= {blogs} title="All Blogs!" />}
+  </div>;
+};
+
+export default Home;
